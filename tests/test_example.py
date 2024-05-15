94984c55ef81ade5fd17383b20fe351d8a6f5dd1
lakrity <nikitaruk@gmail.com>
2024-05-16 02:43:11 +0300

    origin

--- tests/test_example.py
+++ tests/test_example.py
@@ -7,19 +7,6 @@ TOKEN = '9514938c409c7c6bcf7b551a5426bf69'
 HEADER = {'Content-Type' : 'application/json', 'trainer_token': TOKEN}
 TRAINER_ID = 4088
 
-body_registration = {
-    "trainer_token": TOKEN,
-    "email": "ferdal@mail.ru",
-    "password": "Odin123"
-}
-body_confirmation = {
-    "trainer_token" : TOKEN
-}
-body_create = {
-    "name": "Kadavr",
-    "photo": "https://dolnikov.ru/pokemons/albums/048.png"
-}
-
 
 def test_status_code():
     response = requests.get(url=f'{URL}/pokemons', params={'trainer_id': TRAINER_ID})
@@ -29,7 +16,7 @@ def test_pokemon_lvl():
      response_lvl = requests.get(url=f'{URL}/pokemons', params={'trainer_id': TRAINER_ID})
      assert response_lvl.json()['data'][2]['stage'] == '1'
 
-@pytest.mark.parametrize('key, value', [('name', 'Kadavr'),('trainer_id',f'{TRAINER_ID}'),('id', '27298'),('stage', '1')])
+@pytest.mark.parametrize('key, value', [('name', 'New Kadavr'),('trainer_id',f'{TRAINER_ID}'),('id', '27530'),('stage', '1')])
 def test_param(key, value):
     response_param = requests.get(url=f'{URL}/pokemons', params={'trainer_id': TRAINER_ID})
     assert response_param.json()["data"][0][key] == value
